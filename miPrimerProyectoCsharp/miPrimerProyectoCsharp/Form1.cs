9021f21179433e644fb6843e68dd29ecc0d90ecf
Luis Hernandez <Estudiante@DESKTOP-J6D5H3A>
2025-08-13 10:36:00 -0600

    opciones basicas en la calculadora

--- miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
@@ -20,14 +20,53 @@ namespace miPrimerProyectoCsharp {
 
         private void btncalcular_Click(object sender, EventArgs e)
         {
-            double num1, num2, respuesta;
+            double num1, num2, respuesta=0;
             num1 = Double.Parse( txtnum1.Text);
-
             num2 = double.Parse(txtnum2.Text);
 
-            respuesta = num1 + num2;
+            if( optSuma.Checked){
+                respuesta = num1 + num2;
+            }
+            if (optResta.Checked) {
+                respuesta = num1 - num2;
+            }
+            if (optMultiplicacion.Checked) {
+                respuesta = num1 * num2;
+            }
+            if (optDivision.Checked) {
+                respuesta = num1 / num2;
+            }
+            if (optExponente.Checked) {
+                respuesta = Math.Pow(num1, num2);
+            }
+            //Porcentaje.
+            //Factorial. 5! = 5x4x3x2x1=120
+            //Modulo.
 
             lblrespuesta.Text = "Respuesta: "+ respuesta;
+        }
+
+        private void btnCalcularOpciones_Click(object sender, EventArgs e)
+        {
+            double num1, num2, respuesta = 0;
+            num1 = double.Parse(txtnum1.Text);
+            num2 = double.Parse(txtnum2.Text);
+
+            switch (cboOpciones.SelectedIndex) {
+                case 0:
+                    respuesta = num1 + num2;
+                    break;
+                case 1:
+                    respuesta = num1 - num2;
+                    break;
+                case 2:
+                    respuesta = num1 * num2;
+                    break;
+                case 3:
+                    respuesta = num1 / num2;
+                    break;
+            }
+            lblrespuesta.Text = "Respuesta: " + respuesta;
         }
     }
 }
