63568bf20ac281d81749fdf5465785e7691ac5a0
Luis Hernandez <sarco_luis@hotmail.com>
2025-08-21 09:41:44 -0600

    Uso de vectores o array en C#

--- miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.Designer.cs
@@ -24,19 +24,15 @@
         /// </summary>
         private void InitializeComponent() {
             this.btnCalcular = new System.Windows.Forms.Button();
-            this.lblSueldo = new System.Windows.Forms.Label();
-            this.txtSueldo = new System.Windows.Forms.TextBox();
-            this.lblIsss = new System.Windows.Forms.Label();
-            this.lblAfp = new System.Windows.Forms.Label();
-            this.lblIsr = new System.Windows.Forms.Label();
-            this.lblSueldoNeto = new System.Windows.Forms.Label();
-            this.lblTotalDeducciones = new System.Windows.Forms.Label();
+            this.lblSerie = new System.Windows.Forms.Label();
+            this.txtSerie = new System.Windows.Forms.TextBox();
+            this.lblSuma = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnCalcular
             // 
             this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalcular.Location = new System.Drawing.Point(350, 121);
+            this.btnCalcular.Location = new System.Drawing.Point(323, 27);
             this.btnCalcular.Name = "btnCalcular";
             this.btnCalcular.Size = new System.Drawing.Size(173, 80);
             this.btnCalcular.TabIndex = 0;
@@ -44,85 +40,41 @@
             this.btnCalcular.UseVisualStyleBackColor = true;
             this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
-            // lblSueldo
+            // lblSerie
             // 
-            this.lblSueldo.AutoSize = true;
-            this.lblSueldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSueldo.Location = new System.Drawing.Point(24, 56);
-            this.lblSueldo.Name = "lblSueldo";
-            this.lblSueldo.Size = new System.Drawing.Size(98, 24);
-            this.lblSueldo.TabIndex = 1;
-            this.lblSueldo.Text = "SUELDO:";
+            this.lblSerie.AutoSize = true;
+            this.lblSerie.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSerie.Location = new System.Drawing.Point(24, 56);
+            this.lblSerie.Name = "lblSerie";
+            this.lblSerie.Size = new System.Drawing.Size(76, 24);
+            this.lblSerie.TabIndex = 1;
+            this.lblSerie.Text = "SERIE:";
             // 
-            // txtSueldo
+            // txtSerie
             // 
-            this.txtSueldo.Location = new System.Drawing.Point(128, 60);
-            this.txtSueldo.Name = "txtSueldo";
-            this.txtSueldo.Size = new System.Drawing.Size(140, 20);
-            this.txtSueldo.TabIndex = 2;
+            this.txtSerie.Location = new System.Drawing.Point(106, 60);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(198, 20);
+            this.txtSerie.TabIndex = 2;
             // 
-            // lblIsss
+            // lblSuma
             // 
-            this.lblIsss.AutoSize = true;
-            this.lblIsss.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIsss.Location = new System.Drawing.Point(24, 108);
-            this.lblIsss.Name = "lblIsss";
-            this.lblIsss.Size = new System.Drawing.Size(77, 24);
-            this.lblIsss.TabIndex = 3;
-            this.lblIsss.Text = "ISSS: ?";
-            // 
-            // lblAfp
-            // 
-            this.lblAfp.AutoSize = true;
-            this.lblAfp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAfp.Location = new System.Drawing.Point(24, 150);
-            this.lblAfp.Name = "lblAfp";
-            this.lblAfp.Size = new System.Drawing.Size(73, 24);
-            this.lblAfp.TabIndex = 4;
-            this.lblAfp.Text = "AFP: ?";
-            // 
-            // lblIsr
-            // 
-            this.lblIsr.AutoSize = true;
-            this.lblIsr.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIsr.Location = new System.Drawing.Point(24, 193);
-            this.lblIsr.Name = "lblIsr";
-            this.lblIsr.Size = new System.Drawing.Size(65, 24);
-            this.lblIsr.TabIndex = 5;
-            this.lblIsr.Text = "ISR: ?";
-            // 
-            // lblSueldoNeto
-            // 
-            this.lblSueldoNeto.AutoSize = true;
-            this.lblSueldoNeto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSueldoNeto.Location = new System.Drawing.Point(24, 275);
-            this.lblSueldoNeto.Name = "lblSueldoNeto";
-            this.lblSueldoNeto.Size = new System.Drawing.Size(149, 24);
-            this.lblSueldoNeto.TabIndex = 6;
-            this.lblSueldoNeto.Text = "Sueldo Neto: ?";
-            // 
-            // lblTotalDeducciones
-            // 
-            this.lblTotalDeducciones.AutoSize = true;
-            this.lblTotalDeducciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotalDeducciones.Location = new System.Drawing.Point(24, 234);
-            this.lblTotalDeducciones.Name = "lblTotalDeducciones";
-            this.lblTotalDeducciones.Size = new System.Drawing.Size(208, 24);
-            this.lblTotalDeducciones.TabIndex = 7;
-            this.lblTotalDeducciones.Text = "Total Deducciones: ?";
+            this.lblSuma.AutoSize = true;
+            this.lblSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSuma.Location = new System.Drawing.Point(24, 108);
+            this.lblSuma.Name = "lblSuma";
+            this.lblSuma.Size = new System.Drawing.Size(91, 24);
+            this.lblSuma.TabIndex = 3;
+            this.lblSuma.Text = "SUMA: ?";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(566, 373);
-            this.Controls.Add(this.lblTotalDeducciones);
-            this.Controls.Add(this.lblSueldoNeto);
-            this.Controls.Add(this.lblIsr);
-            this.Controls.Add(this.lblAfp);
-            this.Controls.Add(this.lblIsss);
-            this.Controls.Add(this.txtSueldo);
-            this.Controls.Add(this.lblSueldo);
+            this.ClientSize = new System.Drawing.Size(551, 169);
+            this.Controls.Add(this.lblSuma);
+            this.Controls.Add(this.txtSerie);
+            this.Controls.Add(this.lblSerie);
             this.Controls.Add(this.btnCalcular);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -135,13 +87,9 @@
         #endregion
 
         private System.Windows.Forms.Button btnCalcular;
-        private System.Windows.Forms.Label lblSueldo;
-        private System.Windows.Forms.TextBox txtSueldo;
-        private System.Windows.Forms.Label lblIsss;
-        private System.Windows.Forms.Label lblAfp;
-        private System.Windows.Forms.Label lblIsr;
-        private System.Windows.Forms.Label lblSueldoNeto;
-        private System.Windows.Forms.Label lblTotalDeducciones;
+        private System.Windows.Forms.Label lblSerie;
+        private System.Windows.Forms.TextBox txtSerie;
+        private System.Windows.Forms.Label lblSuma;
     }
 }
 
--- miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
+++ miPrimerProyectoCsharp/miPrimerProyectoCsharp/Form1.cs
@@ -15,43 +15,20 @@ namespace miPrimerProyectoCsharp {
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-            //Hacer un ejercicio en C# haciendo uso de matrices, estructuras de repeticion y condicionales, que 
-            //determine el sueldo neto a pagar a un empleado descontando las deducciones correspondientes de ley.
-            //isss, afp, isr.
+            
         }
-        private double[][] tablaIsr = {
-                new Double[] {0.01, 550, 0, 0},
-                new Double[] {550.01, 895.24, 0.10, 17.67},
-                new Double[] {895.25, 2038.10, 0.20, 60},
-                new Double[] {2038.11, 9999999, 0.30, 288.57}
-        };
         private double calcularDeducciones(double sueldo, double porcentaje) {
             return sueldo * porcentaje;
         }
-        private double calcularIsr(double sueldo) { 
-            double isr =0;
-            for (int i = 0; i < tablaIsr.Length; i++) {
-                if (sueldo >= tablaIsr[i][0] && sueldo <= tablaIsr[i][1]) { 
-                    isr = (sueldo - tablaIsr[i][0]) * tablaIsr[i][2] + tablaIsr[i][3];
-                }
-            }
-            return isr;
-        }
         private void btnCalcular_Click(object sender, EventArgs e) {
-            double sueldo = 0, isss = 0, afp = 0, isr = 0, sueldoNeto = 0;
-            sueldo = double.Parse(txtSueldo.Text);
+            String[] serie = txtSerie.Text.Split(','); //5,10,15 => ["5","10","15"] -> String[] m = new String[] { "5", "10", "15" };
 
-            isss = calcularDeducciones(sueldo, 0.03); // 3% de ISSS -> 3/100=0.03
-            afp = calcularDeducciones(sueldo, 0.0725); // 7.25% de AFP -> 7.25/100=0.0725
-            isr = calcularIsr(sueldo - isss - afp); // Calcular ISR
-
-            sueldoNeto = sueldo - isss - afp - isr; // Calcular sueldo neto
-
-            lblIsss.Text = "ISSS: " + isss.ToString("C2");
-            lblAfp.Text = "AFP: " + afp.ToString("C2");
-            lblIsr.Text = "ISR: " + isr.ToString("C2");
-            lblTotalDeducciones.Text = "Total Deducciones: " + (isss + afp + isr).ToString("C2");
-            lblSueldoNeto.Text = "Sueldo Neto: " + sueldoNeto.ToString("C2");
+            int suma = 0;
+            for (int i = 0; i < serie.Length; i++){
+                suma = suma + int.Parse( serie[i]);
+            }
+            lblSuma.Text = "Suma: " + suma;
+            //lblSuma.Text = "Suma: " + serie.Sum();
         }
     }
 }
