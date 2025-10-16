6828cf6b08a92ae4b4ca92beec45840ad7f1d2ed
Luis Hernandez <sarco_luis@hotmail.com>
2025-10-16 10:13:34 -0600

    continuamos con el desarrollo del formulario de alumnos con HTML5 + JS + Bootstrap + JQUERY + UI + ASP.NET + C# + EntityFramework + SQL Server

--- webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
+++ webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b73fbdb06d0d235948c4678870d9b1e38b35da7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f4ad5fbacb9c3ccafdba352a2719c0d6adb29ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
