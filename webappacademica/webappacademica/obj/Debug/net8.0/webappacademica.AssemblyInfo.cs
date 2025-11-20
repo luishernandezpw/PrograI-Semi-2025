60f4d25debe2c12666544238ac239f042484213c
Luis Hernandez <sarco_luis@hotmail.com>
2025-11-20 10:38:23 -0600

    iniciamos el CRUD de matriculas

--- webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
+++ webappacademica/webappacademica/obj/Debug/net8.0/webappacademica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4113539abd4c0da62876ca55f394e874244e29da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab6a2e7252172db014779715541eb8fabd210be9")]
 [assembly: System.Reflection.AssemblyProductAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webappacademica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
